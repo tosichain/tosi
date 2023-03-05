e38a017e6a43ac91c3078fa786c63003069b623b
frehml <frederic.hamelink@hotmail.com>
2023-03-05 01:53:11 +0100

    base link hh config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,18 +10,26 @@ import * as dotenv from "dotenv";
 
 dotenv.config({ path: __dirname + "/.env" });
 
-const RPC_URL = process.env["RPC_URL"] || "https://goerli-rollup.arbitrum.io/rpc";
+const RPC_KEY = process.env["RPC_KEY"] || "https://goerli-rollup.arbitrum.io/rpc";
 const ETHERSCAN_KEY = process.env["ETHERSCAN_KEY"] || "0".repeat(34);
 const PRIVATE_KEY = process.env["PRIVATE_KEY"] || "0".repeat(64);
 
 const config: HardhatUserConfig = {
   networks: {
     arbitrum_goerli: {
-      url: RPC_URL,
+      url: `https://arb-goerli.g.alchemy.com/v2/${RPC_KEY}`,
       accounts: [PRIVATE_KEY],
     },
     arbitrum: {
-      url: RPC_URL,
+      url: `https://arb-mainnet.g.alchemy.com/v2/${RPC_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${RPC_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${RPC_KEY}`,
       accounts: [PRIVATE_KEY],
     },
     hardhat: {
