4f0eb32222ab971a48d2ed1e1923d95ddee02480
nyakiomaina <carolinenyakio21@email.com>
2023-03-16 14:56:40 +0300

    api mocha tests

---
+++ src/client/apiServer.test.ts
@@ -0,0 +1,121 @@
+import * as chai from "chai";
+import { expect } from "chai";
+import chaiHttp from "chai-http";
+
+chai.use(chaiHttp);
+
+describe("GET /api/syncStatus", function () {
+  it("should return the sync status", function (done) {
+    chai
+      .request("http://localhost:30001")
+      .get("/api/syncStatus")
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("isSynced").that.is.a("boolean");
+        done();
+      });
+  });
+  it("should return a 404 error is the blocks dont sync", function (done) {
+    chai
+      .request("http://localhost:30001")
+      .get("/api/syncStatus")
+      .end(function (err, res) {
+        expect(res).to.have.status(404);
+        expect(res.body).to.have.property("error").that.is.a("string");
+        done();
+      });
+  });
+});
+
+describe("GET /api/latestHash", function () {
+  it("should return the latest block hash", function (done) {
+    chai
+      .request("http://localhost:30001")
+      .get("/api/latestBlockHash")
+      .end(function (_err, res) {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("hash").that.is.a("string");
+        done();
+      });
+  });
+
+  it("should return a 404 error if the latest block hash is not found", function (done) {
+    chai
+      .request("http://localhost:30001")
+      .get("/api/latestBlockHash")
+      .end(function (err, res) {
+        expect(res).to.have.status(404);
+        expect(res.body).to.have.property("error").that.equals("none");
+        done();
+      });
+  });
+});
+
+describe("GET /api/latestLocalHash", function () {
+  it("should return the latest local hash", function (done) {
+    chai
+      .request("http://localhost:30001")
+      .get("/api/latestLocalHash")
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("hash").that.is.a("string");
+        done();
+      });
+  });
+  it("should return a 500 error if the latest local hash is not found", function (done) {
+    chai
+      .request("http://localhost:30001")
+      .get("/api/latestLocalHash")
+      .end(function (err, res) {
+        expect(res).to.have.status(500);
+        expect(res.body).to.have.property("error").that.equals("none");
+        done();
+      });
+  });
+});
+
+describe("GET /api/blsPubKeyInHex", function () {
+  it("should return the bls public key in hex", function (done) {
+    chai
+      .request("http://localhost:30001")
+      .get("/api/blsPubKeyInHex")
+      .end(function (_err, res) {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("blsPubKeyInHex").that.is.a("string");
+        done();
+      });
+  });
+  it("should return a 500 error if the bls public key in hex is not found", function (done) {
+    chai
+      .request("http://localhost:30001")
+      .get("/api/blsPubKeyInHex")
+      .end(function (err, res) {
+        expect(res).to.have.status(500);
+        expect(res.body).to.have.property("error").that.is.a("string");
+        done();
+      });
+  });
+});
+
+describe("GET /api/tosiChains", function () {
+  it("should return an array of tosi chains", function (done) {
+    chai
+      .request("http://localhost:30001")
+      .get("/api/tosiChains")
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("tosiChains").that.is.a("array");
+        done();
+      });
+  });
+  it("should return a 500 error if the tosi chains are not found", function (done) {
+    chai
+      .request("http://localhost:30001")
+      .get("/api/tosiChains")
+      .end(function (err, res) {
+        expect(res).to.have.status(500);
+        expect(res.body).to.have.property("error").that.is.a("string");
+        done();
+      });
+  });
+});
