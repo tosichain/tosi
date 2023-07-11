e4d087fba1995b5d46db219d9d5b70b3a0480214
nyakiomaina <carolinenyakio21@email.com>
2023-07-11 15:43:33 +0300

    rewrite shell script to rust- DEBUGGING!

--- src/verifier/grab-and-hash/src/main.rs
+++ src/verifier/grab-and-hash/src/main.rs
@@ -38,19 +38,21 @@ fn main() {
     let metadata = fs::metadata(&tmpfile_path).unwrap();
     let size = metadata.len();
 
-    let log2 = "31";
+    // let log2 = "31";
 
-    let output = Command::new("/app/merkle-tree-hash")
-        .arg("--log2-word-size=3")
-        .arg("--log2-root-size")
-        .arg(&log2)
-        .arg("--log2-leaf-size=12")
-        .arg("--input")
-        .arg(&tmpfile_path)
-        .output()
-        .expect("Failed to run merkle-tree-hash command");
+    // let output = Command::new("/app/merkle-tree-hash")
+    //     .arg("--log2-word-size=3")
+    //     .arg("--log2-root-size")
+    //     .arg(&log2)
+    //     .arg("--log2-leaf-size=12")
+    //     .arg("--input")
+    //     .arg(&tmpfile_path)
+    //     .output()
+    //     .expect("Failed to run merkle-tree-hash command");
 
-    let cartesi_merkle_root = String::from_utf8(output.stdout).unwrap().trim().to_string();
+    // let cartesi_merkle_root = String::from_utf8(output.stdout).unwrap().trim().to_string();
+
+    let cartesi_merkle_root = "00000000000000000000000000000000".to_string();
 
     let _ = Command::new("ipfs")
         .arg("--api")
